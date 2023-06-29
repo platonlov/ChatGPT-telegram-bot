697df167b822662af7b5a60fb7859b633d7f88e6
platonlov <platonloveyko@gmail.com>
2023-06-29 22:47:28 +0700

    Beta 0.0.1 version

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ hello_msg = """
 This bot created for talking with chatGPT
 right from the <b>Telegram!</b>
 """
-openai_api_key = 'Enter_your_openai_api_key'
+openai.api_key = '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'
 tr = Translator()
 
 
@@ -22,6 +22,17 @@ def translate(msg):
     user_text = msg.text
     trans_text = tr.translate(user_text, dest='en')
     print(trans_text.text)
+
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=trans_text.text,
+        max_tokens=600,
+        temperature=0.7
+    )
+    res = response.choices[0].text.strip()
+    trans_res = tr.translate(res, dest='ru')
+
+    tb.send_message(msg.chat.id, trans_res.text)
 
 
 
